d5978f12641bd0136ad39b325de42a01029a4215
sudtanj <sudono007@gmail.com>
2018-07-15 15:54:42 +0700

    adding paper offline database support

---
+++ app/src/main/java/sud_tanj/com/icare/HealthData.java
@@ -0,0 +1,13 @@
+package sud_tanj.com.icare;
+
+/**
+ * This class is part of iCare Project
+ * Any modified within this class without reading the
+ * manual will cause problem!
+ * <p>
+ * Created by Sudono Tanjung on 15/07/2018 - 14:20.
+ * <p>
+ * This class last modified by User
+ */
+public class HealthData {
+}
---
+++ app/src/main/java/sud_tanj/com/icare/OnFragmentInteractionListener.java
@@ -0,0 +1,17 @@
+package sud_tanj.com.icare.iCareFragment;
+
+import android.net.Uri;
+
+/**
+ * This class is part of iCare Project
+ * Any modified within this class without reading the
+ * manual will cause problem!
+ * <p>
+ * Created by Sudono Tanjung on 15/07/2018 - 13:51.
+ * <p>
+ * This class last modified by User
+ */
+public interface OnFragmentInteractionListener {
+    // TODO: Update argument type and name
+    void onFragmentInteraction(Uri uri);
+}
