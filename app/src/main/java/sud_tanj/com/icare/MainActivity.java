88baffd57a3c6d8578d72176641051d908236be6
sudtanj <sudono007@gmail.com>
2018-07-15 13:12:56 +0700

    Initial Commit

---
+++ app/src/main/java/sud_tanj/com/icare/MainActivity.java
@@ -0,0 +1,30 @@
+package sud_tanj.com.icare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.heinrichreimersoftware.materialdrawer.DrawerActivity;
+import com.heinrichreimersoftware.materialdrawer.structure.DrawerItem;
+
+public class MainActivity extends DrawerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        addItem(
+                new DrawerItem()
+                        .setTextPrimary("aaaaaa")
+                        .setOnItemClickListener(new DrawerItem.OnItemClickListener() {
+                            @Override
+                            public void onClick(DrawerItem drawerItem, long id, int position) {
+                                Toast.makeText(MainActivity.this, "Clicked first item #" + id, Toast.LENGTH_SHORT).show();
+                            }
+                        })
+        );
+        addDivider();
+    }
+}
