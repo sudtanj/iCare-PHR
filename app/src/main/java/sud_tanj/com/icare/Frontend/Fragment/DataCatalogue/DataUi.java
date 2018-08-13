99622a346aeb2c80c7191f993ce52c2472b2b8a6
sudtanj <sudono007@gmail.com>
2018-08-13 18:26:35 +0700

    adding comment ui template

--- app/src/main/java/sud_tanj/com/icare/Frontend/Fragment/DataCatalogue/DataUi.java
+++ app/src/main/java/sud_tanj/com/icare/Frontend/Fragment/DataCatalogue/DataUi.java
@@ -15,6 +15,16 @@ import com.mikepenz.fastadapter.FastAdapter;
 import com.mikepenz.fastadapter.IAdapter;
 import com.mikepenz.fastadapter.listeners.OnBindViewHolderListener;
 import com.mikepenz.fastadapter.listeners.OnClickListener;
+import com.nightonke.boommenu.Animation.BoomEnum;
+import com.nightonke.boommenu.Animation.OrderEnum;
+import com.nightonke.boommenu.BoomButtons.BoomButton;
+import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum;
+import com.nightonke.boommenu.BoomButtons.HamButton.Builder;
+import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.ButtonEnum;
+import com.nightonke.boommenu.OnBoomListener;
+import com.nightonke.boommenu.Piece.PiecePlaceEnum;
 import com.ramotion.foldingcell.FoldingCell;
 
 import org.androidannotations.annotations.AfterViews;
@@ -23,7 +33,15 @@ import org.androidannotations.annotations.ViewById;
 
 import java.util.List;
 
+import co.intentservice.chatui.ChatView;
+import co.intentservice.chatui.ChatView.OnSentMessageListener;
+import co.intentservice.chatui.models.ChatMessage;
+import co.intentservice.chatui.models.ChatMessage.Type;
+import me.shaohui.bottomdialog.BottomDialog;
+import me.shaohui.bottomdialog.BottomDialog.ViewListener;
+import sud_tanj.com.icare.Backend.Database.HybridReference;
 import sud_tanj.com.icare.Backend.Database.Monitoring.MonitoringInformation;
+import sud_tanj.com.icare.Backend.Database.PersonalData.DataComment;
 import sud_tanj.com.icare.R;
 
 @EFragment(R.layout.fragment_data_catalogue)
@@ -33,6 +51,7 @@ public class DataUi extends Fragment implements OnClickListener<CellCard>,OnBind
     private FastAdapter fastAdapter;
     protected RecyclerView numberUnit;
     private FirebaseDataAdapter firebaseDataAdapter;
+    private ChatMessage tempMessage;
 
     @AfterViews
     public void init(){
@@ -55,6 +74,8 @@ public class DataUi extends Fragment implements OnClickListener<CellCard>,OnBind
 
         fastAdapter.withOnBindViewHolderListener(this);
         //numberAdapter.withOnBindViewHolderListener(this);
+         */
+
         final BottomDialog bottomDialog=BottomDialog.create(getFragmentManager());
         bottomDialog.setViewListener(new ViewListener() {
             @Override
@@ -62,12 +83,23 @@ public class DataUi extends Fragment implements OnClickListener<CellCard>,OnBind
                 //RecyclerView commentRecyclerView=v.findViewById(R.id.comment_bottomsheet);
                 //commentRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
                 //commentRecyclerView.setAdapter(fastAdapter);
-                BoomMenuButton bmb=v.findViewById(R.id.bmb);
+                final ChatView chatView = (ChatView) v.findViewById(R.id.chat_view);
+                chatView.addMessage(new ChatMessage("aaa",126489, Type.SENT,"Doctor"));
+                final BoomMenuButton bmb=v.findViewById(R.id.bmb);
                 bmb.setBoomEnum(BoomEnum.RANDOM);
                 bmb.setButtonEnum(ButtonEnum.Ham);
                 bmb.setPiecePlaceEnum(PiecePlaceEnum.HAM_2);
                 bmb.setButtonPlaceEnum(ButtonPlaceEnum.HAM_2);
                 bmb.setOrderEnum(OrderEnum.DEFAULT);
+                bmb.setVisibility(View.INVISIBLE);
+                chatView.setOnSentMessageListener(new OnSentMessageListener() {
+                    @Override
+                    public boolean sendMessage(ChatMessage chatMessage) {
+                        bmb.boom();
+                        tempMessage=chatMessage;
+                        return true;
+                    }
+                });
                 bmb.setOnBoomListener(new OnBoomListener() {
                     @Override
                     public void onClicked(int index, BoomButton boomButton) {
@@ -99,21 +131,37 @@ public class DataUi extends Fragment implements OnClickListener<CellCard>,OnBind
                         bottomDialog.dismiss();
                     }
                 });
-                HamButton.Builder builder = new HamButton.Builder()
+                Builder builder = new Builder()
                         .normalText("Post as Doctor")
                         .subNormalText("We will tag this comment as from doctor!")
                         .listener(new OnBMClickListener() {
                             @Override
                             public void onBoomButtonClick(int index) {
+                                HybridReference hybridReference=new HybridReference(
+                                        FirebaseDatabase.getInstance().getReferenceFromUrl(DataComment.KEY)
+                                        .push());
+                                DataComment dataComment=new DataComment();
+                                dataComment.setCommentType(DataComment.COMMENT_BY_DOCTOR);
+                                dataComment.setMessage(tempMessage.getMessage());
+                                hybridReference.setValue(dataComment);
+                                tempMessage=null;
                                 bottomDialog.show();
                             }
                         });
-                HamButton.Builder builder2 = new HamButton.Builder()
+                Builder builder2 = new Builder()
                         .normalText("Post as User")
                         .subNormalText("We will tag this comment as from you!")
                         .listener(new OnBMClickListener() {
                             @Override
                             public void onBoomButtonClick(int index) {
+                                HybridReference hybridReference=new HybridReference(
+                                        FirebaseDatabase.getInstance().getReferenceFromUrl(DataComment.KEY)
+                                                .push());
+                                DataComment dataComment=new DataComment();
+                                dataComment.setCommentType(DataComment.COMMENT_BY_INDIVIDUAL);
+                                dataComment.setMessage(tempMessage.getMessage());
+                                hybridReference.setValue(dataComment);
+                                tempMessage=null;
                                 bottomDialog.show();
                             }
                         });
@@ -124,7 +172,7 @@ public class DataUi extends Fragment implements OnClickListener<CellCard>,OnBind
                 .setLayoutRes(R.layout.layout_slider_cell_bottomsheet_comment)      // dialog layout
         ;
         bottomDialog.show();
-         */
+
         Query query = FirebaseDatabase.getInstance()
                 .getReferenceFromUrl(MonitoringInformation.KEY)
                 .limitToLast(10);
