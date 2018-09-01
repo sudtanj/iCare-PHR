484f2b9c3fe87c6d4f88d5388a40b656566105bf
sudtanj <sudono007@gmail.com>
2018-09-01 12:15:15 +0700

    Signed-off-by: sudtanj <sudono007@gmail.com>

--- app/src/main/java/sud_tanj/com/icare/Frontend/Fragment/DataCatalogue/DataDetail/DataDetails.java
+++ app/src/main/java/sud_tanj/com/icare/Frontend/Fragment/DataCatalogue/DataDetail/DataDetails.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,7 +23,10 @@ import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
 
 import sud_tanj.com.icare.Backend.Database.Monitoring.MonitoringInformation;
 import sud_tanj.com.icare.Backend.Database.PersonalData.HealthData;
@@ -33,6 +40,8 @@ public class DataDetails extends AppCompatActivity implements ValueEventListener
     protected CurrentDataAdapter currentDataAdapter;
     @ViewById(R.id.current_data_recycler_view)
     protected SuperRecyclerView firstRecyclerView;
+    @ViewById(R.id.data_line_chart)
+    protected LineChart lineChartView;
 
     @AfterExtras
     protected void initActionBar(){
@@ -63,6 +72,45 @@ public class DataDetails extends AppCompatActivity implements ValueEventListener
         firstRecyclerView.setAdapter(currentDataAdapter);
     }
 
+    private void initGraph(final MonitoringInformation monitoringInformation){
+        lineChartView.getAxisLeft().setDrawGridLines(false);
+        lineChartView.getXAxis().setDrawGridLines(false);
+        lineChartView.getAxisRight().setDrawGridLines(false);
+        //one month ago milliseconds
+        Calendar c = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        c.add(Calendar.MONTH, -1);
+        long result = c.getTimeInMillis();
+        FirebaseDatabase.getInstance().getReferenceFromUrl(HealthData.KEY)
+                .orderByChild("timeStamp").startAt(result)
+                .endAt(Calendar.getInstance().getTimeInMillis()).addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        List<Entry> entries = new ArrayList<Entry>();
+                        int j=0;
+                        for(DataSnapshot temp:dataSnapshot.getChildren()){
+                            if(monitoringInformation.getHealthDatas().indexOf(temp.getKey())>-1){
+                                HealthData healthData=temp.getValue(HealthData.class);
+                                for(int i=0;i<healthData.getDataList().size();i++) {
+                                    entries.add(new Entry(j,healthData.getDataList().get(i).floatValue()));
+                                }
+                                j++;
+                            }
+                        }
+                        LineDataSet dataSet = new LineDataSet(entries, "Steps");
+                        LineData lineData = new LineData(dataSet);
+                        lineChartView.setData(lineData);
+                        lineChartView.invalidate();
+                     }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                }
+        );
+    }
+
     @OptionsItem(android.R.id.home)
     protected void backButtonClicked(){
         this.onStop();
@@ -72,10 +120,12 @@ public class DataDetails extends AppCompatActivity implements ValueEventListener
     @Override
     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
         MonitoringInformation monitoringInformation=dataSnapshot.getValue(MonitoringInformation.class);
+        setTitle(getString(R.string.details_information_title,monitoringInformation.getName()));
         if(!monitoringInformation.getHealthDatas().isEmpty()) {
             initCurrentData(monitoringInformation.getHealthDatas().get(
                     monitoringInformation.getHealthDatas().size()-1)
                     , monitoringInformation.getGraphLegend());
+            initGraph(monitoringInformation);
         }
     }
 
